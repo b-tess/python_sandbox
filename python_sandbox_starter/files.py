dde36b58098f04f40cf89058bc5839c6a19712cb
Barbara Tess <barbaratessn@gmail.com>
2024-03-13 08:18:15 +0300

    files continued and py_json

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -12,3 +12,13 @@ print('Mode: ', my_file.mode)
 my_file.write('I am learning Python')
 my_file.write(' and this experience is making me realize that I like JavaScript more.')
 my_file.close() #Close the file
+
+#Append to a file
+my_file = open('myFile.txt', 'a') #Using this mode will append to the file
+my_file.write(' I am currently not sure why I am doing any of this...')
+my_file.close()
+
+#Read from the file and print out the first 100 characters
+my_file = open('myFile.txt', 'r+')
+text = my_file.read(100)
+print(text)
--- python_sandbox_starter/py_json.py
+++ python_sandbox_starter/py_json.py
@@ -1,1 +1,13 @@
-# JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+# JSON is commonly used with data APIS. Here's how we can parse JSON into a Python dictionary
+import json
+
+#Convert a JSON to a dictionary => equivalent to JSON.parse() in JS.
+userJSON = '{"first_name": "Jane", "last_name": "Doe", "age": 30}'
+user = json.loads(userJSON) #Parse to a dict
+print(user)
+print(user['first_name'])
+
+#Convert a dictionary to JSON => equivalent to JSON.stringify() in JS.
+car = {'make': 'Ford', 'model': 'Mustang', 'year': 1970}
+carJSON = json.dumps(car)
+print(carJSON)
